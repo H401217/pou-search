6293ea0a7a894414cd3b8d7338667f523a2b9c84
H401217 <104276764+H401217@users.noreply.github.com>
2022-12-06 17:52:49 -0500

    new features

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+--[[
+  _____               _____                     _     
+ |  __ \             / ____|                   | |    
+ | |__) |__  _   _  | (___   ___  __ _ _ __ ___| |__  
+ |  ___/ _ \| | | |  \___ \ / _ \/ _` | '__/ __| '_ \ 
+ | |  | (_) | |_| |  ____) |  __/ (_| | | | (__| | | |
+ |_|   \___/ \__,_| |_____/ \___|\__,_|_|  \___|_| |_|
+                                                      
+Pou Search is an open source project in GitHub (https://github.com/H401217/pou-search).
+]]
+
 _G.Pou = require("pou")
 _G.json = require("json")
 function sColor(p)
@@ -20,7 +31,17 @@ drawPou:drawPou(pouvisit,{color = 1,sz = 0.5})
 	bannerType = "success"
 	bannerMsg = ""
 	
+	logShad = love.graphics.newShader([[
+	vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords ) {
+		vec4 c = Texel(tex,texture_coords);
+		c.w = texture_coords.y*c.w;
+		return c;
+	}
+]])
+
 	icons = {
+		loginBG = love.graphics.newImage("assets/icons/loginBG.png"),
+		logincard = love.graphics.newImage("assets/icons/logincard.png"),
 		success = love.graphics.newImage("assets/icons/success.png"),
 		trouble = love.graphics.newImage("assets/icons/error.png"),
 		lvl = love.graphics.newImage("assets/icons/lvl.png"),
@@ -33,11 +54,18 @@ drawPou:drawPou(pouvisit,{color = 1,sz = 0.5})
 		hunger = love.graphics.newImage("assets/icons/fullness.png"),
 		hp = love.graphics.newImage("assets/icons/health.png"),
 		home = love.graphics.newImage("assets/icons/home.png"),
+		zakeh = love.graphics.newImage("assets/icons/zakeh.png"),
+		insta = love.graphics.newImage("assets/icons/insta.png"),
+		fbico = love.graphics.newImage("assets/icons/fb.png"),
+		twitter = love.graphics.newImage("assets/icons/twitter.png"),
 		b0 = love.graphics.newImage("assets/icons/button0.png"),
+		login = love.graphics.newImage("assets/icons/login0.png"),
+		refresh = love.graphics.newImage("assets/icons/refresh.png"),
 		b1 = love.graphics.newImage("assets/icons/button1.png"),
 		b2 = love.graphics.newImage("assets/icons/button2.png"),
 		b3 = love.graphics.newImage("assets/icons/button3.png"),
 		b4 = love.graphics.newImage("assets/icons/button4.png"),
+		b5 = love.graphics.newImage("assets/icons/button5.png"),
 	}
 	
 	sounds = {
@@ -54,7 +82,7 @@ drawPou:drawPou(pouvisit,{color = 1,sz = 0.5})
 		smolpou = love.graphics.newFont("pou.ttf",20),
 		def = love.graphics.getFont()
 	}
-	state = "login"
+	state = "home"
 	account = {
 		name = "Pou",
 		id = 666,
@@ -73,6 +101,75 @@ drawPou:drawPou(pouvisit,{color = 1,sz = 0.5})
 			cE = 0, --coins Erased
 		},
 	}
+	
+	server = {
+		name = "Pou",
+		description = "Pou Official Server",
+		image = "iVBORw0KGgoAAAANSUhEUgAAAA0AAAANCAIAAAD9iXMrAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAnSURBVChTYyAB1BMChNVtfSYLJAmrgwCoOmIBVBduQFjdCHUfAwMAXG6UCb238f8AAAAASUVORK5CYII=",
+		creator = 98,
+		links = {
+			facebook = "mypou",
+			instagram = "pou.gram",
+			twitter = "poualien",
+			web = "https://www.zakeh.com",
+		}
+	}
+	
+	serverIcon = nil
+	
+	function updateServerImage(base64)
+		local a = love.data.decode("data","base64",base64)
+		local b = love.image.newImageData(a)
+		serverIcon = love.graphics.newImage(b)
+		serverIcon:setFilter("nearest", "nearest")
+		a:release() b:release()
+	end
+	
+	updateServerImage(server.image)
+	
+	configchanged = false
+	
+	function updateServer(js,host)
+		print(js,host,"dos")
+		local _,t = pcall(function() return drawPou.toTable(js) end)
+		if type(t) ~= "table" then t = {} love.window.showMessageBox("Warning!","Metadata for custom server '"..host.."' not found!","warning") end
+		server.name = t.name or "Unnamed"
+		server.description = t.description or ""
+		server.image = t.image or "iVBORw0KGgoAAAANSUhEUgAAAA0AAAANCAIAAAD9iXMrAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAnSURBVChTYyAB1BMChNVtfSYLJAmrgwCoOmIBVBduQFjdCHUfAwMAXG6UCb238f8AAAAASUVORK5CYII="
+		server.creator = t.creator or 0
+		server.links = t.links or {facebook="",instagram="",twitter="",web=""}
+		if host == "http://app.pou.me/" then
+			server = {name = "Pou",description = "Pou Official Server",image = "",creator = 98,links = {	facebook = "mypou",	instagram = "pou.gram",	twitter = "poualien",	web = "https://www.zakeh.com", }}
+		end
+		pcall(function() updateServerImage(server.image) end)
+	end
+	
+	function userChange(option,arg1,arg2)
+		local res = ""
+		if option == "mail" then
+			res = _G.Client.changeEmail(arg1)
+		elseif option == "pass" then
+			res = _G.Client.changePassword(arg1,arg2)
+		elseif option == "name" then
+			res = _G.Client.changeNickname(arg1)
+		end
+		local a,b = pcall(function() return _G.json.decode(res) end)
+		print(res,a,b)
+		if a then
+			if b.error then
+				badState(b.error.message)
+			end
+			if b.success == true then
+				configchanged = true
+				local aa2=arg1
+				if option == "pass" then aa2 = arg2 end
+				local msg = "Changed "..option.." to '"..aa2.."'"
+				goodState(msg)
+			end
+		else
+			badState("An unknown error occurred")
+		end
+	end
 	
 	function badState(msg)
 		sounds.trouble:play()
@@ -112,22 +209,51 @@ drawPou:drawPou(pouvisit,{color = 1,sz = 0.5})
 	
 	function login(e,p,isdata)
 		local client = _G.Pou.login(e,p)
+print(client.type,e,p)
+		local errord = false
 		if client == "no-connection" then badState("No Internet Connection") return false end
+		if client.type ~= "guest" then
+		if client.type == "cookie" then client.me = config.mypou end
+		if client.type == "account" then config.lastlogged = os.time() end
 		local tab = drawPou.toTable(client.me)
 		if tab.error then
 			badState(tab.error.message)
-			return false
+			errord = true
 		else
 			if not isdata then
-				love.filesystem.write("poulogin",e.."¬"..p)
+				config.password = p
+				config.mail = e
+				config.cookie = _G.cookie
+				config.mypou = client.me
+				print(config.mypou)
+				love.filesystem.write("conf.json",_G.json.encode(config))
 			end
-			_G.Client = client
-			state = "home"
+			if _G.Client then if _G.Client.type ~= "guest" then logout() end end
 			updateUser(tab)
-			return true
+		end
+			if tab and tab.n then
+				items.texts.newNick.text = tab.n
+			end
+		end
+		_G.Client = client
+		if errord == true then _G.Client.type = "guest" return end
+		print(_G.Client,"dhsaj")
+		state = "home"
+	end
+	function newNick(new)
+		local res = _G.Client.changeNickname(new)
+		if string.len(res)<1 then return end
+		local t = _G.json.decode(res)
+		if t.error then
+			badState(t.error.message)
+		else
+			goodState("Changed nick to '"..new.."'")
+			account.name = new
+			local tab = _G.json.decode(config.mypou)
+			tab.n = new
+			config.mypou = _G.json.encode(tab)
 		end
 	end
-	
 	function like()
 		local res = _G.Client.like(account.id)
 		if string.len(res)<1 then return end
@@ -153,6 +279,10 @@ drawPou:drawPou(pouvisit,{color = 1,sz = 0.5})
 		end
 	end
 	function visit(info)
+		if _G.Client.type == "guest" then
+			state = "login"
+			return
+		end
 		print(info)
 		local tab
 		if not info then
@@ -187,24 +317,76 @@ drawPou:drawPou(pouvisit,{color = 1,sz = 0.5})
 		end
 		state = "top"
 	end
-	function logout()
+	
+	function changeHost(newHost)
+		local a = love.window.showMessageBox("Warning!","Changing host will close your current Pou account!\nAre you sure to continue?",{"No","Yes!",escapebutton=1},"warning")
+		if a == 2 then
+			logout(true)
+			local res = _G.Pou.changeHost(newHost)
+			config.host = newHost
+			love.filesystem.write("conf.json",_G.json.encode(config))
+			print(res,newHost)
+			updateServer(res,newHost)
+		else
+			--not
+		end
+	end
+	
+	function relogin()
+		if _G.Client.type ~= "guest" then
+			local mail,pass = config.mail,config.password
+			logout()
+			love.timer.sleep(1)
+			login(mail,pass)
+		else
+			badState("Can't relogin while not logged in")
+		end
+	end
+	
+	function logout(force)
 		local s = _G.Client.logOut()
-		if s then
+		if s or force==true then
 			goodState("Success!")
-			love.filesystem.write("poulogin","")
-			_G.Client = nil
+			config.mail = "" config.password = "" config.cookie = "" config.mypou = "" love.filesystem.write("conf.json",_G.json.encode(config))
+			_G.Client.type = "guest"
 			items.texts.mail.text = ""
 			items.texts.pass.text = ""
 			state = "login"
 		end
 	end
-	local c,s = love.filesystem.read("poulogin")
-	if c then
+	--local c,s = love.filesystem.read("poulogin")
+	conf,_suc = love.filesystem.read("conf.json")
+	local temptab = {fastlog = false,mail = "",password = "",cookie = "",mypou="",host="http://app.pou.me/",lastlogged=0}
+	if (not conf) or (string.len(conf)==0) then
+		love.filesystem.write("conf.json",_G.json.encode(temptab)) config = temptab
+	else
+		_sus,config = pcall(function() return json.decode(conf) end)
+		if _sus == false then
+			love.filesystem.write("conf.json",_G.json.encode(temptab))
+			config = temptab
+			login()
+		else
+			local a = _G.Pou.changeHost(config.host)
+			if a and config.host ~= "http://app.pou.me/" then updateServer(a,config.host) end
+			if #config.mail > 0 and #config.password > 0 then
+				if #config.cookie > 0 and #config.mypou > 0 then
+					login(config.cookie,nil,true)
+				else
+					login(config.mail,config.password,true)
+				end
+			else
+				login()
+			end
+		end
+	end
+	
+	--[[if c then
 		local find = string.find(c,"¬") or 0
 		local mail = string.sub(c,0,find-1)
 		local pass = string.sub(c,find+2,1000)
 		local sus = login(mail,pass,true)
-	end
+	end]]
+	--changeHost("http://app.pou.me/")
 end
 
 function love.keypressed(key)
@@ -233,12 +415,17 @@ function love.update(dt)
 	if state == "login" then
 		items.texts.mail.v = true items.texts.mail.e = true items.texts.pass.v = true items.texts.pass.e = true
 		items.buttons.login.v = true items.buttons.login.e = true
+		items.buttons.exit.e = true
 	elseif state == "home" then
 		for a,b in pairs(items.buttons) do
 			if string.match(a,"button") then
 				b.e = true
 			end
 		end
+		items.buttons.zakehweb.e = true
+		items.buttons.instagram.e = true
+		items.buttons.facebook.e = true
+		items.buttons.twitter.e = true
 	elseif state == "visit" then
 		items.buttons.like.e = true
 		items.buttons.exit.v = false items.buttons.exit.e = true
@@ -250,6 +437,19 @@ function love.update(dt)
 		items.buttons.sMail.v = true items.buttons.sMail.e = true
 		items.buttons.sID.v = true items.buttons.sID.e = true
 		items.buttons.exit.v = false items.buttons.exit.e = true
+	elseif state == "conf" then
+		for a,b in pairs(items.buttons) do
+			if string.match(a,"conf") then
+				b.e = true
+				b.v = true
+			end
+		end
+		items.buttons.exit.v = false items.buttons.exit.e = true
+		items.texts.host.v = true items.texts.host.e = true
+		items.texts.newNick.v = true items.texts.newNick.e = true
+		items.texts.newMail.v = true items.texts.newMail.e = true
+		items.texts.newPass.v = true items.texts.newPass.e = true
+		items.texts.oldPass.v = true items.texts.oldPass.e = true
 	elseif state == "top" then
 		items.buttons.exit.v = false items.buttons.exit.e = true
 	end
@@ -259,6 +459,12 @@ function love.draw()
 	love.graphics.setBackgroundColor(0.3,0.6,1)
 	love.graphics.setFont(fonts.pou)
 	love.graphics.print("Pou",love.graphics:getWidth()/2-fonts.pou:getWidth("Pou")/2,10)
+	
+	--bg
+	love.graphics.setShader(logShad)
+	love.graphics.draw(icons.loginBG,0,200,0,800/1000,800/1000)
+	love.graphics.setShader()
+	
 	if state == "visit" then
 		love.graphics.draw(icons.home,items.buttons.exit.x,items.buttons.exit.y,0,items.buttons.exit.sx/icons.home:getWidth(),items.buttons.exit.sy/icons.home:getHeight())
 		love.graphics.setFont(fonts.medpou)
@@ -305,17 +511,70 @@ function love.draw()
 		love.graphics.setColor(0,0,0,1)
 		love.graphics.print(account.lvl,530-fonts.def:getWidth(account.lvl)/2,287.5-fonts.def:getHeight()/2)
 	elseif state == "home" then
-		love.graphics.draw(icons.b0,items.buttons.button0.x,items.buttons.button0.y,0,items.buttons.button0.sx/icons.b0:getWidth(),items.buttons.button0.sy/icons.b0:getHeight())
+		if _G.Client then
+			if _G.Client.type ~= "guest" then
+				love.graphics.draw(icons.b0,items.buttons.button0.x,items.buttons.button0.y,0,items.buttons.button0.sx/icons.b0:getWidth(),items.buttons.button0.sy/icons.b0:getHeight())
+				love.graphics.setFont(fonts.smolpou)
+				local time = os.time()-config.lastlogged
+				local str = " seconds"
+				if time >= 60 then
+					time = math.floor(time/60)
+					str = " minutes"
+					if time >= 60 then
+						time = math.floor(time/60)
+						str = " hours"
+						if time >= 24 then
+							time = math.floor(time/24)
+							str = " days"
+						end
+					end
+				end
+				love.graphics.print("Last logged in: "..time..str,110,565)
+				love.graphics.setFont(fonts.pou)
+			else
+				love.graphics.draw(icons.login,items.buttons.button0.x,items.buttons.button0.y,0,items.buttons.button0.sx/icons.login:getWidth(),items.buttons.button0.sy/icons.login:getHeight())
+			end
+		end
 		love.graphics.draw(icons.b1,items.buttons.button1.x,items.buttons.button1.y,0,items.buttons.button1.sx/icons.b1:getWidth(),items.buttons.button1.sy/icons.b1:getHeight())
 		love.graphics.draw(icons.b2,items.buttons.button2.x,items.buttons.button2.y,0,items.buttons.button2.sx/icons.b2:getWidth(),items.buttons.button2.sy/icons.b2:getHeight())
 		love.graphics.draw(icons.b3,items.buttons.button3.x,items.buttons.button3.y,0,items.buttons.button2.sx/icons.b3:getWidth(),items.buttons.button3.sy/icons.b3:getHeight())
 		love.graphics.draw(icons.b4,items.buttons.button4.x,items.buttons.button4.y,0,items.buttons.button2.sx/icons.b4:getWidth(),items.buttons.button4.sy/icons.b4:getHeight())
+		love.graphics.draw(icons.b5,items.buttons.button5.x,items.buttons.button5.y,0,items.buttons.button5.sx/icons.b5:getWidth(),items.buttons.button5.sy/icons.b5:getHeight())
+		love.graphics.draw(icons.zakeh,items.buttons.zakehweb.x,items.buttons.zakehweb.y,0,items.buttons.zakehweb.sx/icons.zakeh:getWidth(),items.buttons.zakehweb.sy/icons.zakeh:getHeight())
+		love.graphics.draw(icons.insta,items.buttons.instagram.x,items.buttons.instagram.y,0,items.buttons.instagram.sx/icons.insta:getWidth(),items.buttons.instagram.sy/icons.insta:getHeight())
+		love.graphics.draw(icons.fbico,items.buttons.facebook.x,items.buttons.facebook.y,0,items.buttons.facebook.sx/icons.fbico:getWidth(),items.buttons.facebook.sy/icons.fbico:getHeight())
+		love.graphics.draw(icons.twitter,items.buttons.twitter.x,items.buttons.twitter.y,0,items.buttons.twitter.sx/icons.twitter:getWidth(),items.buttons.twitter.sy/icons.twitter:getHeight())
+		if os.time()-config.lastlogged >= 86400 then
+			love.graphics.setColor(0,1,0,1)
+		end
+		love.graphics.draw(icons.refresh,items.buttons.button.x,items.buttons.button.y,0,items.buttons.button.sx/icons.refresh:getWidth(),items.buttons.button.sy/icons.refresh:getHeight())
+		love.graphics.setColor(1,1,1,1)
 	elseif state == "search" then
 		love.graphics.draw(icons.home,items.buttons.exit.x,items.buttons.exit.y,0,items.buttons.exit.sx/icons.home:getWidth(),items.buttons.exit.sy/icons.home:getHeight())
 	elseif state == "top" then
 		love.graphics.draw(icons.home,items.buttons.exit.x,items.buttons.exit.y,0,items.buttons.exit.sx/icons.home:getWidth(),items.buttons.exit.sy/icons.home:getHeight())
+		love.graphics.setColor(0.5,0.5,0.5,0.8)
+		love.graphics.rectangle("fill",170,130,470,450)
 		love.graphics.setFont(fonts.smolpou)
+		love.graphics.setColor(1,1,1,1)
 		love.graphics.print(ttl1..ttl2,200,150)
+	elseif state == "login" then
+		love.graphics.draw(icons.home,items.buttons.exit.x,items.buttons.exit.y,0,items.buttons.exit.sx/icons.home:getWidth(),items.buttons.exit.sy/icons.home:getHeight())
+		love.graphics.draw(icons.logincard,50,200,0,200/icons.logincard:getWidth(),200/icons.logincard:getHeight())
+	elseif state == "conf" then
+		--heads
+		love.graphics.setFont(fonts.smolpou)
+		love.graphics.print("Change Nickname",130,100)
+		love.graphics.print("Change Email",130,165)
+		love.graphics.print("Change Password",130,230)
+		love.graphics.print("Change Server",400,350)
+		--content
+		love.graphics.draw(icons.home,items.buttons.exit.x,items.buttons.exit.y,0,items.buttons.exit.sx/icons.home:getWidth(),items.buttons.exit.sy/icons.home:getHeight())
+		love.graphics.draw(serverIcon,400,100,0,150/serverIcon:getWidth(),150/serverIcon:getHeight())
+		love.graphics.print(server.name,560,100)
+		love.graphics.setFont(fonts.def)
+		love.graphics.print(server.description,560,120)
+		love.graphics.print("Host: "..config.host.."\nFacebook: "..server.links.facebook.."\nTwitter: "..server.links.twitter.."\nInstagram: "..server.links.instagram.."\nWebsite: "..server.links.web,410,260)
 	end
 	love.graphics.setColor(1,1,1,1)
 	love.graphics.setFont(fonts.def)
