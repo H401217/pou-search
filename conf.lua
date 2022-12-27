575c5388f0f07bd78a13cd930f29b98d40de98a4
H401217 <104276764+H401217@users.noreply.github.com>
2022-12-27 18:17:12 -0500

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -2,5 +2,7 @@ function love.conf(t)
 	t.window.title = "Pou search!"
 	t.window.icon = "assets/icons/lvl.png"
 	t.window.resizable = false
+	t.identity = "pouserver"
+	--t.window.vsync = 0
 	t.version = "11.4"
 end
